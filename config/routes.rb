6f37e350083479e77955fff2cffabbd1a6171e50
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-27 11:01:59 +0300

    commit tree 01459d3d6028797c1bc8d367af30cb9d317846ef

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create a route for making a GET request to /students
+
+  get '/students', to: 'students#index'
+  get '/students/grades', to: 'students#grades'
+  get '/students/highest_grade', to: 'students#highest_grade'
 end
